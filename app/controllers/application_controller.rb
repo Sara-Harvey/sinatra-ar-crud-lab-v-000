73a9e0ae3dfd576bbac96bf53f37a7bb1d4c4f83
Sara Harvey <saraharveynyc@gmail.com>
2019-09-07 21:39:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,8 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/articles' do 
+    @article = Article.create(params)
+    redirect to "articles/#{@article.id}"
     erb :show 
   end
 end
