15316379ed1c566a38cf30f1db13a406925ba29d
Sara Harvey <saraharveynyc@gmail.com>
2019-09-07 21:41:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/articles/:id" do 
-    @article=Article.find(params[:id])
+    @article = Article.find(params[:id])
     erb :show 
   end 
 
